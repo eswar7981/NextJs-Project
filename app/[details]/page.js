f341310bb8c913b79895a58b2225534be611efec
eswar7396 <eswarsatyavarapu7981@gmail.com>
2023-10-28 20:05:10 +0530

    getstaticprops

--- app/[details]/page.js
+++ app/[details]/page.js
@@ -3,13 +3,40 @@ import React from 'react'
 
 const page = ({params}) => {
     const data=params.details
-
   return (
     <div>
       <MeetupDetail id={data} />
       
     </div>
   )
+
+
+  
 }
+export async function getStaticPaths(){
+    
+    return {
+        fallback:false,
+        paths:[{
+            params:{
+                meetupId:'1',
+            }
+        }]
+    }
+}
+
+export async function getStaticProps(context){
+    
+    const meetupId=context.params.details
+    console.log(meetupId)
+    return {
+        props:{
+            meetupdata:{
+                id:meetupId
+            }
+        }
+    }
+}
+
 
 export default page
